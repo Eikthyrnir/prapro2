5f5ace08adbbbbb2b34eb0713c4f0bf7c3a6ba38
Eikthyrnir <timbarvenov@gmail.com>
2023-01-08 12:25:02 +0100

    TaskRepo now also supports CRUD

--- src/main/java/com/timbar/wmi/prapro2/repositories/TaskRepo.java
+++ src/main/java/com/timbar/wmi/prapro2/repositories/TaskRepo.java
@@ -1,7 +1,10 @@
 package com.timbar.wmi.prapro2.repositories;
 
 import com.timbar.wmi.prapro2.entities.Task;
+import org.springframework.data.repository.CrudRepository;
 import org.springframework.data.repository.PagingAndSortingRepository;
 
-public interface TaskRepo extends PagingAndSortingRepository<Task, Integer> {
+public interface TaskRepo extends
+        CrudRepository<Task, Integer>,
+        PagingAndSortingRepository<Task, Integer> {
 }
